be3691c0392169fc8aad1fd02fd6e48cd11ccf3c
Chris Morgan <chris.morganiser@gmail.com>
2010-04-22 20:33:22 +1000

    Manual: neater, briefer and more useful INI and environment index entries.

--- Other/Source/Manual/_ext/paldocs.py
+++ Other/Source/Manual/_ext/paldocs.py
@@ -1,5 +1,6 @@
 def setup(app):
-    app.add_crossref_type('ini-section', 'ini-section', indextemplate='pair: INI section; %s')
-    app.add_crossref_type('ini-key', 'ini-key', indextemplate='pair: INI key; %s')
-    app.add_crossref_type('env', 'env', indextemplate='pair: environment variable; %s')
-    # I know that there's 'envvar' but it chokes a bit (warning) on colons due to its index template
+    app.add_crossref_type('ini-section', 'ini-section', indextemplate='single: %s')
+    app.add_crossref_type('ini-key', 'ini-key', indextemplate='single: %s')
+    app.add_crossref_type('env', 'env', indextemplate='single: %%%s%%')
+    # I know that there's 'envvar' (indextemplate 'environment variable; %s'
+    # but it's no good with colons (warning) and isn't this compact
