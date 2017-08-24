43f8d46360cb051133af2465cf73b4dfbefb68a5
Gord Caswell <ve4jhj@gmail.com>
2017-08-24 17:15:49 -0500

    Update template to matching with current help documentation/main website

--- Other/Source/Manual/conf.py
+++ Other/Source/Manual/conf.py
@@ -121,12 +121,12 @@ html_theme = 'classic'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = '_static/help_logo_top.png'
+html_logo = '_static/Help_Logo_Top.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = '_static/favicon.ico'
+html_favicon = '_static/Favicon.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
